731cfacf3efb8e30bca955654dd947016f4a74ed
Nail Khabibullin <xobnail@gmail.com>
2024-10-31 13:40:38 +0300

    add dockerfile

---
+++ MicroBook.Client.Contracts/UpdateBook.cs
@@ -0,0 +1,16 @@
+﻿namespace MicroBook.Contracts;
+
+/// <summary>
+/// Contract for updating book after order.
+/// </summary>
+public record UpdateBook
+{
+    /// <summary>
+    /// Book Id.
+    /// </summary>
+    public int BookId { get; init; }
+    /// <summary>
+    /// Number of books ordered 
+    /// </summary>
+    public int Amount { get; init; }
+}
